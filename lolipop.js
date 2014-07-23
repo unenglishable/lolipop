86cb1469fe032c45950df9e891f00851b4433055
unenglishable <unenglishable@gmail.com>
2014-07-22 17:04:08 -1000

    update implementation of showTables
    this implementation no longer specifies lolipopdb as the exact database
    to read from.  rather, it uses a configuration instance...
    it was strange to do it the other way

--- lolipop.js
+++ lolipop.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
-exports.showTables = function (err, config, callback) {
+exports.showTables = function (err, db_config, callback) {
   if (err) {
     return callback(err);
   }
 
-  var connection = mysql.createConnection(config['lolipopdb']);
+  var connection = mysql.createConnection(db_config);
   var tables = [];
 
   connection.query('SHOW tables', function (err, rows) {
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var lolipop = require('./lolipop');
 var config = require('./config');
 
-lolipop.showTables(null, config, function (err, tables) {
+lolipop.showTables(null, config['lolipopdb'], function (err, tables) {
   if (err) {
     throw err;
   }
