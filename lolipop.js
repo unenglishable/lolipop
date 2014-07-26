2c7a447c73629e324627cc08165861a79b2db966
unenglishable <unenglishable@gmail.com>
2014-07-25 16:37:43 -1000

    add methods for columns and rows + streaming

--- lolipop.js
+++ lolipop.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
 var config = require('./config');
 var dbConfig = config.lolipopdb;
+var connection = mysql.createConnection(dbConfig);
 
 exports.showTables = function (err, callback) {
   if (err) {
     return callback(err);
   }
 
-  var connection = mysql.createConnection(dbConfig);
   var tables = [];
 
   connection.query('SHOW tables', function (err, rows) {
@@ -16,6 +16,29 @@ exports.showTables = function (err, callback) {
     });
     callback(null, tables);
   });
+}
 
-  connection.end();
+exports.showColumns = function (err, table, callback) {
+  if (err) {
+    return callback(err);
+  }
+
+  connection.query('SHOW columns FROM ' + mysql.escapeId(table), function (err, rows) {
+    if (callback && typeof(callback) === "function") {
+      callback(null, rows);
+    }
+  });
+}
+
+exports.streamRows = function (err, table) {
+  return connection.query('SELECT * FROM ' + mysql.escapeId(table));
+}
+
+exports.end = function (callback) {
+  if (callback && typeof(callback) === "function") {
+    connection.end(callback());
+  }
+  else {
+    connection.end();
+  }
 }
